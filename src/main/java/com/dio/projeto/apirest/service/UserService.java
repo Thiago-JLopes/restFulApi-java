b8a3bd88ac8e57f4f09ee57760d8f5198e080789
Thiago Lopes <tiagojosecnn@outlook.com>
2024-06-24 22:09:25 -0300

    conclui camada de negocios.

--- src/main/java/com/dio/projeto/apirest/service/UserService.java
+++ src/main/java/com/dio/projeto/apirest/service/UserService.java
@@ -1,2 +1,8 @@
-package com.dio.projeto.apirest.service;public interface UserService {
+package com.dio.projeto.apirest.service;
+
+import com.dio.projeto.apirest.model.User;
+
+public interface UserService {
+    User findById(Long id);
+    User create(User userToCreate);
 }
--- src/main/java/com/dio/projeto/apirest/service/imp/UserServiceImp.java
+++ src/main/java/com/dio/projeto/apirest/service/imp/UserServiceImp.java
@@ -1,2 +1,31 @@
-package com.dio.projeto.apirest.service.imp;public class UserServiceImp {
+package com.dio.projeto.apirest.service.imp;
+
+import com.dio.projeto.apirest.model.User;
+import com.dio.projeto.apirest.repository.UserRepository;
+import com.dio.projeto.apirest.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImp implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImp(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if(userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This account number already exists");
+        }
+        return userRepository.save(userToCreate);
+    }
 }
